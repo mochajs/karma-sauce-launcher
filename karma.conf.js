c4add8bf3e247a91c91d47aa3a8cee1d62594a27
Christopher Hiller <boneskull@boneskull.com>
2017-06-03 16:00:07 -0700

    pull into mochajs org

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,7 @@
 module.exports = function (config) {
   if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
-    console.log('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.');
+    console.log(
+      'Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.');
     process.exit(1);
   }
 
@@ -29,11 +30,18 @@ module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine'],
-    files: [
-      'src/*.js',
-      'test/*.js'
+    plugins: [
+      'karma-jasmine',
+      require.resolve('./')
     ],
-    reporters: ['progress', 'saucelabs'],
+    files: [
+      'examples/src/*.js',
+      'examples/test/*.js'
+    ],
+    reporters: [
+      'progress',
+      'saucelabs'
+    ],
     port: 9876,
     colors: true,
     sauceLabs: {
@@ -44,8 +52,7 @@ module.exports = function (config) {
         logfile: 'sauce_connect.log'
       },
       public: 'public'
-    },
-    // Increase timeout in case connection in CI is slow
+    }, // Increase timeout in case connection in CI is slow
     captureTimeout: 120000,
     customLaunchers: customLaunchers,
     browsers: Object.keys(customLaunchers),
